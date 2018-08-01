87c3b4e12f82a1e39860faac024e24b414d05e09
吴梦超 <>
2018-08-01 17:07:08 +0800

    init

---
+++ dvaApp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import dva, { connect } from 'dva-no-router'
+import App from './App';
+import model from './model';
+// 1. Initialize
+const app = dva();
+
+// 2. Model
+app.model(model);
+
+// 3. Router
+/*app.router(() => <Router />);*/
+
+// 4. Start
+export default () => {
+    return app.start(<App/>)
+}
---
+++ model.js
@@ -0,0 +1,12 @@
+export default{
+    namespace:"model",
+    state:{
+
+    },
+    effects:{
+
+    },
+    reducers:{
+
+    }
+}
